41809adf24c70ecde8a4b17e5fdfe9918f337952
M Riezky Nurfajri <riezkynurfajri@gmail.com>
2022-08-12 09:04:21 +0700

    server hit test

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,6 @@
 const express = require("express")
-const router = express.Router
+const router = express.Router()
+
+router.get("/", (req, res) => res.status(200).json("WELCOME"))
 
 module.exports = router
